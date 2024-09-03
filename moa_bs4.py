04e42d541a5070c269073089125c6868dedeeef2
nryberg <nryberg@gmail.com>
2024-09-03 06:34:39 -0500

    Finally got a line on the listings and date

--- moa_bs4.py
+++ moa_bs4.py
@@ -1,6 +1,15 @@
 from re import X
 from bs4 import BeautifulSoup
 
+# Remove extra unprintable characters and spaces
+def clean_text(dirty_text):
+    working = dirty_text.replace("\t", "")
+    working = working.replace("\n", "")
+    working = working.replace("\r", "")
+    working = ' '.join(working.split())
+
+    return working
+
 running_category = ''
 tenant = ''
 running = 0
@@ -8,25 +17,33 @@ running = 0
 with open("./pages/2006_06_02_Mall of America - Mall Directory.htm") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
+capture_date = soup.find('td', id="displayMonthEl")
+mall_date = ' '.join(capture_date.get("title").split('here:')[1:]).strip()
+# print(capture_date.get("title").split(':')[1:])
+print(mall_date)
+
 listingContents = soup.findAll("div", id="listingContent")
 
 for listingContent in listingContents:
     # print(listingContent.prettify())
     if listingContent and listingContent.find("table"):
-        listings = listingContent.find("table")
-        print(listings.prettify())
-        for row in listings.select("tr"):
-            testnode = row.find("strong")
+        sections = listingContent.select("div > table > tbody")
 
-            if testnode != None:
-                running_category = testnode.get_text()
-            elif testnode == None:
-                # print(row.prettify())
-                tenants = row.select("tr > td > table > tbody > tr > td > a")
-                for tenant in tenants:
-                    running += 1
-                    tenant_value = tenant.get_text()
-                    print(running, running_category, tenant_value) # tenant.get_text())
-                    if running >= 10 :
-                        break
+        for section in sections:
+        # print(listings.prettify())
+            for row in section.find_all("tr", recursive=False):
+                #category_node = row.find("strong")
+
+                if row.find("strong"):
+                    running_category = clean_text(row.find("strong").get_text())
+
+                if row.select("tr > td > table > tbody > tr > td > a"):
+
+                    tenants = row.select("tr > td > table > tbody > tr > td > a")
+                    for tenant in tenants:
+                        running += 1
+                        tenant_value = clean_text(tenant.get_text())
+                        # print(running, ":",  running_category, "- " , tenant_value) # tenant.get_text())
+                # if running >= 10 :
+                #     break
 # print(listings.prettify())
