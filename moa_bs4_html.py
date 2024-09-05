fa44c260596ffedcd96e4430e4b43686c613a0fd
nryberg <nryberg@gmail.com>
2024-09-05 16:57:20 -0500

    My god

--- moa_bs4_html.py
+++ moa_bs4_html.py
@@ -18,10 +18,15 @@ def clean_text(dirty_text):
 
 def is_regular_adddress(s):
     # Regular expression to match an uppercase letter followed by exactly three digits
-    pattern = r'^[A-Z]\d{3}$'
+    pattern = r'^[A-Za-z]\d{3}$'
 
     # Use re.match to check if the string matches the pattern
     return bool(re.match(pattern, s))
+
+def is_parens_address(s):
+    pattern = r'^\(.*\)$'
+    return bool(re.match(pattern, s))
+
 
 def split_camel_case(s):
     # Use a regular expression to find transitions between lowercase to uppercase or digits
@@ -59,23 +64,29 @@ def process_file(file_path, csv_writer):
 
             for section in sections:
                 for row in section.find_all("tr", recursive=False):
-                    #category_node = row.find("strong")
 
+
+
+                    #category_node = row.find("strong")
+                    # Is this a section header?
                     if row.find("strong"):
                         running_category = clean_text(row.find("strong").get_text())
 
+                    # Is this a store listing section?
                     if row.select("tr > td > table > tbody > tr > td > a"):
 
-                        tenants = row.select("tr > td > table > tbody > tr > td > a")
-                        for tenant in tenants:
+                        tenant_links = row.select("tr > td > table > tbody > tr > td > a")
+                        for tenant in tenant_links:
+                            store = ''
+                            address = ''
+
                             running += 1
 
-                            # Store information is just a bear
-
                             store_url_raw = tenant['href']
-                            store_url_address_arr = store_url_raw.split('_')[5:]
-                            tenant_value = clean_text(tenant.get_text())
-                            tv_arr = tenant_value.split()
+                            store_url_arr = store_url_raw.split('_')[5:]
+
+                            store_text_raw = clean_text(tenant.get_text())
+                            store_text_arr = store_text_raw.split()
 
                             # Addressing is a complete mix
                             # If there's a valid address at the end of the
@@ -85,16 +96,49 @@ def process_file(file_path, csv_writer):
                             # - Test for `park` for park based
                             # [ ] TODO - Fix
 
-                            test_addr = tv_arr[-1]
+                            store_text_test_addr = store_text_arr[-1]
 
-                            if is_regular_adddress(test_addr):
-                                store = ' '.join(tv_arr[0:-1])
-                                address = test_addr
+                            # Fix parenthetical addresses
+
+                            store_url_test = store_url_arr[-1].split('.')[0]
+
+                            if store_text_raw.__contains__('Julius'):
+                                print("Raw Text: ", store_text_raw)
+                                print("Text Address Test: ", store_text_test_addr)
+                                print("Text Array:", store_text_arr)
+                                print("Raw URL:", store_url_raw)
+                                print("URL_Test:", store_url_test)
+                                print("URL Arr:", store_url_arr)
+
+
+
+                            if store_url_test == 'tenant':  # Department Stores
+                                store = store_text_raw
+                                address = 'Anchor'
+                            elif store_url_test == 'park':  # Theme Park Stores
+                                store = store_text_raw
+                                address = 'Theme Park'
+                            elif store_url_test == 'Underwater':
+                                store = store_text_raw
+                                address = 'Underwater World'
+                            elif is_regular_adddress(store_text_test_addr):
+                                store = ' '.join(store_text_arr[0:-1])
+                                address = store_text_test_addr
+                            elif is_parens_address(store_text_test_addr):
+                                store = ' '.join(store_text_arr[0:-1])
+                                address = re.sub(r'^\(|\)$', '', store_text_test_addr)
                             else:
-                                address = store_url_address_arr[-1].split('.')[0]
-                                if not is_regular_adddress(address):
-                                    address_arr = split_camel_case(address)
+                                if is_regular_adddress(store_url_test):
+                                    store = store_text_raw
+                                    address = store_url_test
+                                else:
+
+                                    address_arr = split_camel_case(store_url_test)
                                     address = ' '.join(address_arr)
+                                    store = store_text_raw
+
+                            if running_category == 'Specialty Retail - Carts' and address == '':
+                                address = 'Cart'
 
                             # Build the array for output
                             data_out = []
